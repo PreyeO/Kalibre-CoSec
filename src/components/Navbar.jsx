d96f565bb7d59e56826b345b5d5b6e7c5bb18960
PreyeO <omusukup@yahoo.com>
2025-03-22 13:06:21 +0100

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo1 from "../../public/1.svg";
 
 const Navbar = () => {
   return (
-    <nav className=" absolute w-full md:h-[80px] h-[60px] flex items-center lg:px-[80px] px-[20px]  ">
+    <nav className=" absolute w-full md:h-[80px] h-[60px] flex items-center lg:px-[80px] px-[20px] mt-3  ">
       <img src={Logo1} alt="logo" className="w-60 h-60 mt-3" />
     </nav>
   );
